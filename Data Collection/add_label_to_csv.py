f863e50229a3601b5e0199b5ace646a11cb40b8f
GuneetTalwar <guneetsinghtalwar@gmail.com>
2022-10-25 16:52:26 +0530

    Updated labels to csv file

--- Data Collection/add_label_to_csv.py
+++ Data Collection/add_label_to_csv.py
@@ -3,28 +3,33 @@ import json
 import dateutil.parser as dp
 
 
-json_file_path = "Subject_1_EPOCFLEX_164574_2022.10.18T14.53.22+05.30.json"
-csv_file_path =  "Subject_1_EPOCFLEX_164574_2022.10.18T14.53.22+05.30.csv"
-labels_to_index = {'rest':0, 'grab':1, 'elbow_up':2, 'elbow_down':3, 'release':4}
+json_file_path = "Data/Day_1/Subject_1_EPOCFLEX_164574_2022.10.18T14.53.22+05.30.json"
+csv_file_path =  "Data/Day_1/Subject_1_EPOCFLEX_164574_2022.10.18T14.53.22+05.30.csv"
+labels_to_index = {'rest':1, 'grab':2, 'elbow_up':3, 'elbow_down':4, 'release':5, 'None': 0}
+
+
+def labelsToIndex(label):
+    return labels_to_index[label]
 
 def returnLabel(timestamp): #This code will check which label range does the current timestamp belong to and return the labels name
 
 
     with open(json_file_path, 'r') as j:
-         contents = json.loads(j.read())
-         for i in contents['Markers']:
-             startDT = dp.parse(i['startDatetime']).timestamp()
-             endDT = dp.parse(i['endDatetime']).timestamp()
-             if startDT <= timestamp <= endDT:
-                 return i['label']
-             else:
-                 print("ok")
+        i=0
+        data = json.loads(j.read())
+        while i!=len(data['Markers'])-2:
+           startDT = dp.parse(data['Markers'][i]['startDatetime']).timestamp()
+           endDT = dp.parse(data['Markers'][i+1]['startDatetime']).timestamp()
+           print(startDT,endDT,timestamp)
+           if startDT <= timestamp <= endDT:
+               return data['Markers'][i]['label']
+           i=i+1
+    return "None"
 
 
 
 df = pd.read_csv(csv_file_path,skiprows=1) #skipping first row for original column names
 
 #Next two lines will generated a new column, one will have name of label and other will have index corresponding to label
-df['Movement Label Name'] = returnLabel(df['Timestamp'])
-df['Movement Label Index'] = labels_to_index[df['Movement Label Name']]
-#df.to_csv(csv_file_path)
+df['Movement Label Name'] = df.apply(lambda row: returnLabel(row['Timestamp']), axis=1)
+df['Movement Label Index'] = df.apply(lambda row: labelsToIndex(row['Movement Label Name']), axis=1)
