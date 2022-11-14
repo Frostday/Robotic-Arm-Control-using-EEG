3da4c1885f6ca4646a95d9e2b462f8ffcb18f036
GuneetTalwar <guneetsinghtalwar@gmail.com>
2022-11-14 15:34:19 +0530

    Added threadpool

--- Data Collection/model.py
+++ Data Collection/model.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+import concurrent.futures as cf
+from cf import ThreadPoolExecutor
 
 X, batch = [], []
 batches = 32
@@ -7,6 +9,7 @@ timesteps = 10
 
 def send_to_model(data):
     global X, batch
+    model = tf.keras.models.load_model('eeg_model.h5')
     new_data = data['eeg'][2:34]
     X.append(new_data)
     if len(X) < timesteps:
@@ -15,11 +18,12 @@ def send_to_model(data):
         batch.append(X)
         X = []
         if len(batch) == batches:
-            model = tf.keras.models.load_model('eeg_model.h5')
-            batches = np.array(batches)
-            movements = model.predict(batches)
-            print(movements)
-            # call function to move arm
+            with ThreadPoolExecutor(max_workers=1) as ex:
+                batches = np.array(batches)
+                future = ex.submit(model.predict(batches))
+                #movements = model.predict(batches)
+                future.result()
+                # call function to move arm
     else:
         print("error")
         return
